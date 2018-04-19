711488df0bf4d6b9621837454d791ab558c58ccc
Arnadi Denanda <arz1893@gmail.com>
2018-04-19 12:26:05 +0700

    Updated on April 19 2018

--- public/js/chartjs/feedback_product/all/feedback_product_report_all_yearly.js
+++ public/js/chartjs/feedback_product/all/feedback_product_report_all_yearly.js
@@ -6,8 +6,9 @@ if($('#feedback_product_chart_all_yearly').length > 0) {
     var tenantId = $('#tenantId').val();
     var year = $('#select_year').val();
     var count = $('#show_data').val();
+    window.rating = 3;
     console.log({tenantId: tenantId, year: year, count: count });
-    const url = window.location.protocol + "//" + window.location.host + '/api/feedback_product_report/' + tenantId + '/get-all-report-yearly/' + '3' + '/' + year + '/' + count;
+    const url = window.location.protocol + "//" + window.location.host + '/api/feedback_product_report/' + tenantId + '/get-all-report-yearly/' + rating + '/' + year + '/' + count;
 
     axios.get(url).then(response => {
         console.log(response.data);
@@ -54,53 +55,97 @@ if($('#feedback_product_chart_all_yearly').length > 0) {
         onChangeParameter();
     });
 
+    function customerRating(selected) {
+        $('i.smiley_rating').each(function (index, element) {
+            $(element).removeClass('is-selected');
+        });
+        $(selected).addClass('is-selected');
+        let rating = $(selected).data('value');
+        window.rating = rating;
+        myChart.destroy();
+        switch (rating) {
+            case 1 : {
+                $('input[name=customer_rating]').attr('checked',false);
+                $('#radio_dissatisfied').attr('checked', 'checked');
+                window.feedbackLabel = "Dissatisfied";
+                window.bgColor = "rgba(255, 77, 77, 0.7)";
+                onChangeParameter();
+                break;
+            }
+            case 2: {
+                $('input[name=customer_rating]').attr('checked',false);
+                $('#radio_neutral').attr('checked', 'checked');
+                window.feedbackLabel = "Neutral";
+                window.bgColor = "rgba(230, 184, 0, 0.7)";
+                onChangeParameter();
+                break;
+            }
+            case 3: {
+                $('input[name=customer_rating]').attr('checked',false);
+                $('#radio_satisfied').attr('checked', 'checked');
+                window.feedbackLabel = "Satisfied";
+                window.bgColor = "rgba(109, 167, 247, 0.7)";
+                onChangeParameter();
+                break;
+            }
+        }
+    }
+
     function onChangeParameter() {
         var ctx = document.getElementById("feedback_product_chart_all_yearly");
         var tenantId = $('#tenantId').val();
         var year = $('#select_year').val();
         var count = $('#show_data').val();
         $('#current_year').text($('#select_year').val());
-        const url = window.location.protocol + "//" + window.location.host + '/api/feedback_product_report/' + tenantId + '/get-all-report-yearly/' + '3' + '/' + year + '/' + count;
+        const url = window.location.protocol + "//" + window.location.host + '/api/feedback_product_report/' + tenantId + '/get-all-report-yearly/' + rating + '/' + year + '/' + count;
+        $('#loading_state').removeClass('invisible');
 
-        axios.get(url).then(response => {
-            if(response.data.error === undefined) {
-                $('#not_found').css('display', 'none');
-                $('#feedback_product_chart_all_yearly').css({'display': '', 'height': '55vh', 'width': '80vw'});
-                let myChart = new Chart(ctx, {
-                    type: 'bar',
-                    data: {
-                        labels: response.data.labels,
-                        datasets: [{
-                            label: 'Satisfied',
-                            data: response.data.data,
-                            backgroundColor: 'rgba(109, 167, 247, 0.7)',
-                            borderWidth: 1,
-                        }]
-                    },
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero:true,
-                                    fontSize: 10
-                                }
-                            }],
-                            xAxes: [{
-                                ticks: {
-                                    maxRotation: 90,
-                                    fontSize: 10
-                                }
+        function sendRequest() {
+            axios.get(url).then(response => {
+                if(response.data.error === undefined) {
+                    $('#not_found').css('display', 'none');
+                    $('#loading_state').addClass('invisible');
+                    let myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: response.data.labels,
+                            datasets: [{
+                                label: feedbackLabel,
+                                data: response.data.data,
+                                backgroundColor: bgColor,
+                                borderWidth: 1,
                             }]
+                        },
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero:true,
+                                        fontSize: 10
+                                    }
+                                }],
+                                xAxes: [{
+                                    ticks: {
+                                        maxRotation: 90,
+                                        fontSize: 10
+                                    }
+                                }]
+                            }
                         }
-                    }
-                });
-                window.myChart = myChart;
-            } else {
-                $('#not_found').css('display', '');
-                $('#feedback_product_chart_all_yearly').css('display', 'none');
-            }
-        }).catch(error => {
-            console.log(error);
-        });
+                    });
+                    window.myChart = myChart;
+                    $('#feedback_product_chart_all_yearly').css({'display': ''});
+                } else {
+                    $('#not_found').css('display', '');
+                    $('#loading_state').addClass('invisible');
+                    $('#feedback_product_chart_all_yearly').css('display', 'none');
+                }
+            }).catch(error => {
+                console.log(error);
+            });
+        }
+
+        let debounceFunction = _.debounce(sendRequest, 1000);
+        debounceFunction();
     }
 }
--- resources/views/report/feedback_product/feedback_product_report_all.blade.php
+++ resources/views/report/feedback_product/feedback_product_report_all.blade.php
@@ -1,8 +1,8 @@
 @extends('home')
 
 @push('scripts')
-    <!-- Axios -->
     <script src="{{ asset('js/axios/axios.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/lodash/lodash.js') }}" type="text/javascript"></script>
     <script src="{{ asset('js/chartjs/feedback_product/all/feedback_product_report_all_yearly.js') }}" type="text/javascript"></script>
 @endpush
 
@@ -26,32 +26,37 @@
         <a role="button" class="btn btn-xs btn-default">Monthly</a>
         <a role="button" class="btn btn-xs btn-default active">Yearly</a>
     </div>
+    {{ Form::radio('customer_rating', 1, false, ['id' => 'radio_dissatisfied', 'class' => 'invisible']) }}
+    {{ Form::radio('customer_rating', 2, false, ['id' => 'radio_neutral', 'class' => 'invisible']) }}
+    {{ Form::radio('customer_rating', 3, false, ['id' => 'radio_satisfied', 'class' => 'invisible', 'checked' => true]) }}
 
     <div class="text-center">
-        Customer Rating <br>
-        <div class="btn-group" role="group" aria-label="...">
-            <button type="button" class="btn btn-default" id="dissatisfied">
-                <i class="smiley_rating material-icons text-red"
-                   style="font-size: 2em;"
-                   data-value="1">
-                    sentiment_very_dissatisfied
-                </i>
-            </button>
-            <button type="button" class="btn btn-default" id="neutral">
-                <i class="smiley_rating material-icons text-yellow"
-                   style="font-size: 2em;"
-                   data-value="2">
-                    sentiment_neutral
-                </i>
-            </button>
-            <button type="button" class="btn btn-default active" id="satisfied">
-                <i class="smiley_rating material-icons text-green"
-                   style="font-size: 2em;"
-                   data-value="3">
-                    sentiment_very_satisfied
-                </i>
-            </button>
-        </div>
+        Customer Rating
+        <br>
+        <a>
+            <i id="very_bad"
+               class="smiley_rating material-icons text-maroon"
+               style="font-size: 3.5em;"
+               data-value="1" onclick="customerRating(this)">
+                sentiment_very_dissatisfied
+            </i>
+        </a>
+        <a>
+            <i id="normal"
+               class="smiley_rating material-icons text-yellow"
+               style="font-size: 3.5em;"
+               data-value="2" onclick="customerRating(this)">
+                sentiment_neutral
+            </i>
+        </a>
+        <a>
+            <i id="very_satisfied"
+               class="smiley_rating material-icons text-green is-selected"
+               style="font-size: 3.5em;"
+               data-value="3" onclick="customerRating(this)">
+                sentiment_very_satisfied
+            </i>
+        </a>
     </div>
 
     <div class="row">
@@ -76,11 +81,17 @@
         </div>
     </div>
 
+    <div id="loading_state" class="text-center invisible">
+        <i class="fa fa-circle-o-notch fa-spin"></i> Loading...
+    </div>
+
     <div id="not_found" class="well" style="margin-top: 3%; display: none;">
         <div class="text-center">
             There is no report found at current year
         </div>
     </div>
 
-    <canvas id="feedback_product_chart_all_yearly" style="position: relative; right: 5%; height:55vh; width:80vw"></canvas>
+    <div class="container-fluid">
+        <canvas id="feedback_product_chart_all_yearly" style="height:55vh; width:80vw"></canvas>
+    </div>
 @endsection
