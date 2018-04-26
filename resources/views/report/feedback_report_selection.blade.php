e097844197b7ab94fa731fd24fa2c74b5cee56fa
Arnadi Denanda <arz1893@gmail.com>
2018-04-26 14:50:30 +0700

    Updated on April 26 2018

--- resources/views/report/feedback_report_selection.blade.php
+++ resources/views/report/feedback_report_selection.blade.php
@@ -18,32 +18,52 @@
                 <img class="img-responsive img-rounded" src="{{ asset('default-images/37.svg') }}">
             </div>
 
-            <div class="col-md-5 col-sm-4">
+            <div class="col-lg-6 col-md-5 col-sm-4">
                 <h4 class="text-danger">Welcome to Feedback Report Section</h4>
                 <p align="justify">This is a template that is great for small businesses. It doesn't have too much fancy flare to it, but it makes a great use of the standard Bootstrap core components. Feel free to use this template for any project you want!</p>
+
+                <div class="row">
+                    <div class="col-lg-6">
+                        <a role="button">
+                            <div class="panel panel-default">
+                                <div class="panel-body text-center">
+                                    All
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-6">
+                        <a role="button">
+                            <div class="panel panel-default">
+                                <div class="panel-body text-center">
+                                    Tag
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-6">
+                        <a role="button">
+                            <div class="panel panel-default">
+                                <div class="panel-body text-center">
+                                    Product
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-6">
+                        <a role="button">
+                            <div class="panel panel-default">
+                                <div class="panel-body text-center">
+                                    Service
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
             </div>
             <!-- /.col-md-4 -->
         </div>
         <!-- /.row -->
-        <hr>
-        <!-- Content Row -->
-        <div class="row">
-            <div class="col-md-6">
-                <h2 class="text-info">Feedback Product Report <i class="fa fa-line-chart"></i></h2>
-                <p>View all feedback product report</p>
-                <a href="{{ route('feedback_product_report.index') }}" class="btn btn-danger">
-                    Show <i class="fa fa-arrow-circle-right"></i>
-                </a>
-            </div>
-            <div class="col-md-6">
-                <h2 class="text-info">Feedback Service Report <i class="fa fa-line-chart"></i></h2>
-                <p>View all feedback service report</p>
-                <a href="{{ route('feedback_service_report.index') }}" class="btn btn-warning" role="button">
-                    Show <i class="fa fa-arrow-circle-right"></i>
-                </a>
-            </div>
-        </div>
-        <!-- /.row -->
-
     </div>
 @endsection
